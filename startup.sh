de67348daffdf2776cfaacbb79db00b472598103
HauntedHecarim <rdevine86@gmail.com>
2025-02-07 19:26:38 -0500

    startup.sh

--- startup.sh
+++ startup.sh
@@ -1,21 +1,23 @@
-#!/bin/sh
-# Update the system package lists
+#!/bin/bash
+
+# Install system dependencies required for pyodbc
+apt-get update && apt-get install -y \
+    unixodbc \
+    unixodbc-dev \
+    odbcinst \
+    libpq-dev \
+    curl \
+    gnupg
+
+# Install Microsoft ODBC Driver 17 (for Azure SQL)
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
-
-# Install necessary dependencies for PyODBC and system requirements
-apt-get install -y git unixodbc-dev unixodbc gcc g++ build-essential libssl-dev
-
-# Define the deployment directory
-APP_DIR="/home/site/wwwroot"
-
-# Navigate to the app directory
-cd $APP_DIR
-
-# Pull the latest code from GitHub
-git pull https://github.com/HauntedHecarim/cd1756-Azure-Applications-project.git main
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
 # Install Python dependencies
-pip install --no-cache-dir -r requirements.txt
+pip install -r requirements.txt
 
-# Run the application using Gunicorn
-gunicorn -b 0.0.0.0:8000 FlaskWebProject:app
+# Start the application
+gunicorn --bind 0.0.0.0:8000 FlaskWebProject:app
+
